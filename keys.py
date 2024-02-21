3937d69898368809fb29baa0aa58d8e28e8226be
sanjaykumardev <duderaj1976@gmail.com>
2024-02-21 14:20:06 +0530

    first commit

---
+++ keys.py
@@ -0,0 +1,5 @@
+account_sid = 'ACfa835bbd99c433a6b393f153acee6a16'
+auth_token = '65956699fad1f2c6dddd669770eb281c'
+
+twilio_number = '+13156442484'
+my_phone_number  = '+919841365940'
---
+++ keys2.py
@@ -0,0 +1,5 @@
+account_sid = 'AC987dde4a1596e8e65f499872f7fe1851'
+auth_token = 'de26828752e7b331e4231d83dde663d0'
+
+twilio_number1 = '+13158175224'
+my_phone_number2  = '+918610626654'
